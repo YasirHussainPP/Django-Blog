58da782c80015459a5e80c935fe14407ace7ed0a
Yasir Hussain <yasir.hussain@numrex.com>
2025-12-11 17:52:25 +0500

    Add the home page and also the categories tab

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from blogs.models import Category, Blog
 
 def home(request):
-    return render(request, 'home.html')
+
+    featured_posts = Blog.objects.filter(is_featured=True,status="published").order_by('-created_at')
+    posts=Blog.objects.filter(is_featured=False,status="published").order_by('-created_at')[:5]
+
+    context = {
+        'featured_posts': featured_posts,
+        'posts': posts,
+    }
+    return render(request, 'home.html',context)
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from .models import Blog,Category
 
 # Create your views here.
+
+def post_by_category(request,category_id):
+    posts=Blog.objects.filter(status='published',category=category_id)
+    try:
+        categoy=Category.objects.get(pk=category_id)
+    except:
+        return redirect('home')
+    context = {
+        'posts':posts,
+        'categoy':categoy,
+    }
+    return render(request, 'posts_by_category.html',context)
